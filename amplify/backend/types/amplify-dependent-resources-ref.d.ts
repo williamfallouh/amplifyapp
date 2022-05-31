50166eea926471535331a363ff3f1744d140ef7c
FALLOUH William <william.fallouh@soprasteria.com>
2022-05-31 16:29:16 +0200

    added notes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s32f6b2c8f": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
