cc1cf5d2743a1d20e120200d4ffbbefc83f09069
FALLOUH William <william.fallouh@soprasteria.com>
2022-06-01 10:10:28 +0200

    used the var

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
 
 
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
